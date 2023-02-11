67d1b62235f10c607e56e568c989d65f9b00c113
Michael Roberson <michaelroberson@gmail.com>
2023-02-11 18:32:10 -0500

    Add home page JS.

---
+++ static/js/home.js
@@ -0,0 +1,6 @@
+const justiceButton = document.getElementById("justice-button");
+const wooo = document.getElementById("wooo");
+
+justiceButton.addEventListener("click", () => {
+  wooo.play();
+});
