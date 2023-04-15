ea180372ce93531e82c25905ebebd7b09d02293a
Michael Roberson <michaelroberson@gmail.com>
2023-04-15 18:31:09 -0400

    Add modal on home page for new course.

--- static/js/home.js
+++ static/js/home.js
@@ -4,3 +4,8 @@ const wooo = document.getElementById("wooo");
 justiceButton.addEventListener("click", () => {
   wooo.play();
 });
+
+window.onload = () => {
+  const modal = new bootstrap.Modal('#courseModal');
+  modal.show();
+}
